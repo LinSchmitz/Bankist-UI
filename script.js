5dea50af175019582bb72eb3343eee8d65fa9eb7
naleilan <leilanschmitz@gmail.com>
2026-05-05 09:10:40 +0400

    page navigation

--- script.js
+++ script.js
@@ -48,6 +48,8 @@ btnScrollTo.addEventListener("click", function (e) {
 document.querySelectorAll(".nav__link").forEach(function (el) {
   el.addEventListener("click", function (e) {
     e.preventDefault();
+    const id = this.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   });
 });
 
