c38a4ea96edb7f6e8e9ff250971e1cbe7ee4059c
naleilan <leilanschmitz@gmail.com>
2026-05-07 09:57:51 +0400

    Added lazy loading image

--- script.js
+++ script.js
@@ -149,9 +149,12 @@ const loadImg = function (entries, observer) {
 
   if (!entry.isIntersecting) return;
 
-  // Replace src with
+  // Replace src with data-src
   entry.target.src = entry.target.dataset.src;
-  entry.target.classList.remove("lazy-img");
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+  observer.unobserve(entry.target);
 };
 
 const imgObserver = new IntersectionObserver(loadImg, {
